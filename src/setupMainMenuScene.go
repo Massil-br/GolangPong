0abb42f1978f68817aa74feb24d6eb3df58aa50a
Massil <160241880+Massil-br@users.noreply.github.com>
2025-08-05 00:39:34 +0200

    separated each object declaration

--- src/setupMainMenuScene.go
+++ src/setupMainMenuScene.go
@@ -2,40 +2,11 @@ package src
 
 import (
 	engine "github.com/Massil-br/GolangPong/src/Engine"
-	time "github.com/Massil-br/GolangPong/src/Engine/Time"
-	"github.com/Massil-br/GolangPong/src/scripts"
+	mainmenu "github.com/Massil-br/GolangPong/src/MainMenu"
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 func SetupMainMenu(targetScene *engine.Scene, font *rl.Font) {
-	//gameObject
-	title := &engine.GameObject{Name: "Title",
-		Active: true,
-		Transform: engine.Transform2D{
-			Position: rl.NewVector2(0, 0),
-			Rotation: 0,
-			Scale:    rl.NewVector2(1, 1),
-		},
-		FrameData: &time.Data,
-	}
-	//component
-	text := &scripts.Text{
-		Parent:      title,
-		Font:        font,
-		Text:        "Massil's Pong",
-		Position:    title.Transform.Position,
-		FontSize:    title.FrameData.Height / 15,
-		Spacing:     title.FrameData.Height / 15 / 8,
-		Color:       rl.White,
-		TextSize:    rl.NewVector2(0, 0),
-		DivFontSize: 15,
-		DivSpacing:  8,
-		PXPourcent:  50,
-		PYPourcent:  20,
-	}
-	//add component to gameobject
-	title.AddComponent(text)
-	//add gamobject to scene
-	targetScene.AddObject(title)
 
+	mainmenu.SetupTitle(targetScene, font)
 }
